46ebcc5a65184d38a8aa025072056d446c1a84e0
bartn1k <bartek.slupik@gmail.com>
2021-08-23 17:27:57 +0200

    Added Arduino debugger code

---
+++ 6502_debugger/6502_debugger.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+
+#define RX 1
+#define TX 0
+
+#define CLK 2
+#define RST 3
+#define RWB 4
+
+#define EXP1 0x20
+#define EXP2 0x21
+#define IODIRA 0x00
+#define IODIRB 0x01
+#define GPIOA 0x12
+#define GPIOB 0x13
+#define GPPUA 0x0c
+#define GPPUB 0x0d
+
+
+void setRegister(byte board, byte reg, byte data){
+  Wire.beginTransmission(board);
+  Wire.write(reg);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+
+byte readRegister(byte board, byte reg){
+  Wire.beginTransmission(board);
+  Wire.write(reg);
+  Wire.requestFrom((int) board, 1);
+  byte res = (byte) Wire.read();
+  Wire.endTransmission();
+  return res;
+}
+
+
+void onClock(){
+  byte addrMS = readRegister(EXP1, GPIOA);
+  byte addrLS = readRegister(EXP1, GPIOB);
+  byte data = readRegister(EXP2, GPIOB);
+  byte VIA = readRegister(EXP2, GPIOA);
+  int rwb = digitalRead(RWB);
+
+  char buf[30] = {0};
+  sprintf(buf, "%02x%02x  %c %02x  VIA_B=%02x", addrMS, addrLS, rwb==HIGH?'r':'w', data, VIA);
+  Serial.print(buf);
+  Serial.println();
+}
+
+
+void onReset(){
+  Serial.print("---------- RESET ----------");
+  Serial.println();
+}
+
+
+void setup()
+{
+ Serial.begin(9600);
+ Wire.begin();
+ Serial.print("Serial initialized.");
+ Serial.println();
+ 
+ setRegister(EXP1, IODIRA, 0xFF);
+ setRegister(EXP1, IODIRB, 0xFF);
+ setRegister(EXP2, IODIRA, 0xFF);
+ setRegister(EXP2, IODIRB, 0xFF);
+
+ pinMode(CLK, INPUT);
+ pinMode(RST, INPUT);
+ pinMode(RWB, INPUT);
+}
+
+int old_clk = LOW;
+int old_rst = LOW;
+
+void loop(){   
+  int rst = digitalRead(RST);
+  if (rst == LOW && old_rst == HIGH) {
+    onReset();
+  }
+  old_rst = rst;
+  
+  int clk = digitalRead(CLK);
+  if (clk == HIGH && old_clk == LOW) {
+      onClock();
+  }
+  old_clk = clk;
+  delay(50);
+}
